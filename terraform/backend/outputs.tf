68a48af4d20c329ddbb526d6f5c9ee4957b3cd5e
Alexander Sorokin <10476593-SorokinAlexander@users.noreply.gitlab.com>
2022-09-14 13:39:19 +0300

    Updating CI\CD Terraform

--- terraform/backend/outputs.tf
+++ terraform/backend/outputs.tf
@@ -1,4 +1,4 @@
-output "s3_static_website" {
-  value = aws_s3_bucket.static_web_site.website_endpoint
-  description = "Domain name of the website"
-}
+# output "s3_static_website" {
+#   value = aws_s3_bucket.static_web_site.website_endpoint
+#   description = "Domain name of the website"
+# }
